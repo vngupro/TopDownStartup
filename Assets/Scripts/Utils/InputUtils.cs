e4dfa49be8e848ea77b23430f119325f63f23aa4
Julien QUENTIN <jqntn88@gmail.com>
2023-03-16 23:18:10 +0100

    add InputUtils

---
+++ Assets/Scripts/Utils/InputUtils.cs
@@ -0,0 +1,8 @@
+namespace Utils
+{
+    public static class InputUtils
+    {
+        public const string AXIS_HORIZONTAL = "Horizontal";
+        public const string AXIS_VERTICAL = "Vertical";
+    }
+}
