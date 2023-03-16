2125bbd61df1880095ec1ccff74656d98700519e
Alex Psychameron Copin <toretankskilled@gmail.com>
2023-03-16 09:27:33 +0100

    Add Heal Event

--- Assets/Scripts/HealthSystem/HealthModule.cs
+++ Assets/Scripts/HealthSystem/HealthModule.cs
@@ -3,7 +3,21 @@ using UnityEngine;
 
 public class HealthModule : MonoBehaviour
 {
+    /// <summary>
+    /// Bind to add desired effects to Damage, Call ApplyDamage(float) when you want to Apply Damage.
+    /// Died is invoked when _health <= 0 automatically when applying damage.
+    /// Don't forget to unbind (-=) 
+    /// </summary>
     public event Action<float> Damaged;
+    /// <summary>
+    /// Bind to add desired effects to Heal, Call ApplyHeal(float) when you want to Apply Heal.
+    /// Don't forget to unbind (-=) 
+    /// </summary>
+    public event Action<float> Healed;
+    /// <summary>
+    /// Bind (+=) to add desired effects to Died, Call ApplyHeal(float) when you want to Apply Heal.
+    /// Don't forget to unbind (-=) 
+    /// </summary>
     public event Action Died;
 
     [SerializeField] private int _initHealth;
@@ -16,5 +30,11 @@ public class HealthModule : MonoBehaviour
         {
             Died?.Invoke();
         }
+    }
+
+    public void ApplyHeal(float f)
+    {
+        _health = Mathf.Clamp(_health + f, _health, _initHealth);
+        Healed?.Invoke(f);
     }
 }
