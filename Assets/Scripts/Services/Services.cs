e351546b73f067152bf5107782153de63fba95b3
Julien QUENTIN <jqntn88@gmail.com>
2023-03-19 18:37:35 +0100

    add awake safety

--- Assets/Scripts/Services/Services.cs
+++ Assets/Scripts/Services/Services.cs
@@ -1,12 +1,22 @@
+using System;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 using VContainer;
 using VContainer.Unity;
 
 internal class Services : LifetimeScope
 {
+    private const string AUTHORIZED_CALLER = "Awake";
+
     private static IObjectResolver _container;
 
-    public static T Resolve<T>() => _container.Resolve<T>();
+    public static T Resolve<T>([CallerMemberName] string caller = default)
+    {
+        if (caller != AUTHORIZED_CALLER)
+            throw new InvalidOperationException($"Must Resolve through {AUTHORIZED_CALLER}");
+
+        return _container.Resolve<T>();
+    }
 
     protected override void Configure(IContainerBuilder builder)
     {
