089ad6c58003a156473b6168011379323bf05210
Julien QUENTIN <jqntn88@gmail.com>
2023-03-19 19:08:06 +0100

    add .ctor resolve support

--- Assets/Scripts/Dummy.cs
+++ Assets/Scripts/Dummy.cs
@@ -7,4 +7,11 @@ internal sealed class Dummy : MonoBehaviour
     private void Awake() => _yoService ??= Services.Resolve<IYoService>();
 
     private void Start() => _yoService.Yo();
+
+    private void OnEnable() => new Dummy1();
+}
+
+internal sealed class Dummy1
+{
+    public Dummy1() => Services.Resolve<IYoService>().Yo();
 }
