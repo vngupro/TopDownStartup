7a864b8acbc19f71b77747287864b587b3aaea20
Kévin Roussel <roussel@intechinfo.fr>
2023-04-03 22:54:51 +0200

    Linq torment with pokemon

---
+++ Assets/_POKEMON/Moves.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class Moves : MonoBehaviour
+{
+    public int accuracy;
+    public string category;
+    public string cname;
+    public string ename;
+    public int id;
+    public string jname;
+    public int power;
+    public int pp;
+    public string types;
+}
